3355b16ccef448437e1b53a76ce537a7a6dbfc6b
Ezgi <ezgiiiaslannn@outlook.com>
2021-07-19 16:28:52 +0300

    Update PlayerCollisionController.cs

--- RunnerEAI/Assets/_Scripts/PlayerCollisionController.cs
+++ RunnerEAI/Assets/_Scripts/PlayerCollisionController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Player.Collision
+public class PlayerCollisionController : MonoBehaviour
 {
-
+    private Vector3 slm;
 }
