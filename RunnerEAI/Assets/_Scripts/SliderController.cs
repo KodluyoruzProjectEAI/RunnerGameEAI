c49660196190ae0879ae34f270855ec14c592e80
Ataberk Uysal <54504620+ataberkuysal@users.noreply.github.com>
2021-07-25 15:35:21 +0300

    Special Ability

--- RunnerEAI/Assets/_Scripts/SliderController.cs
+++ RunnerEAI/Assets/_Scripts/SliderController.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SliderController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Image coffee;
+    
+    private float stamina;
+    private float maxStamina;
+    
     void Start()
     {
-        
+        stamina = 0;
+        maxStamina = 100;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        stamina = Mathf.Clamp(stamina, 0, maxStamina);
+        coffee.fillAmount =stamina / maxStamina; // fraction needed because fillAmount is 0 to 1 in value
+    }
+
+    public void Stamina(int integer)
+    {
+        if (integer == 1)
+        {
+            stamina += 25;
+        }
+        else if (integer == -1)
+        {
+            stamina -= 25;
+        } 
     }
 }
